d5736540a7e53be4827e581b9839e2ebadacc4e4
brandondiaz <branal845diaz@gmail.com>
2023-12-30 03:03:18 -0600

    Logica para simulación de puerto serial implementada

--- volmetro.py
+++ volmetro.py
@@ -2,17 +2,38 @@ import serial
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import random
+import time
 
-# Configurar la conexión serial
-ser = serial.Serial('COM3', 19200)  # Ajusta a tu configuración
+# Flag para controlar el modo de simulación
+SIMULATED = True
 
-def leer_serial():
+def leer_serial_simulado():
+    """Esta función simula la lectura de datos desde un puerto serial."""
+    # Simula la lectura de un valor binario aleatorio
+    simulated_bin_value = random.randint(0, 255)
+    # Calcula el voltaje simulado
+    voltaje_simulado = (simulated_bin_value * 235.2941) / 1000
+    return voltaje_simulado
+
+def leer_serial_real(ser):
+    """Esta función lee los datos reales desde un puerto serial."""
     # Leer desde serial y decodificar
     linea = ser.readline().decode('utf-8').rstrip()
     # Convertir a valor de voltaje
     valor_binario = int(linea)  # Asegúrate de que los datos sean correctos
-    voltaje = (valor_binario * 19.6078) / 1000  # Ajustar según tus cálculos
+    voltaje = (valor_binario * 235.2941) / 1000  # Ajustar según tus cálculos
     return voltaje
+
+def leer_serial():
+    """Esta función decide si leer desde el puerto real o simular la lectura."""
+    if SIMULATED:
+        return leer_serial_simulado()
+    else:
+        ser = serial.Serial('COM3', 19200)  # Ajusta a tu configuración
+        return leer_serial_real(ser)
+
+# Configurar la conexión serial
 
 # Configurar la ventana de Tkinter
 root = tk.Tk()
