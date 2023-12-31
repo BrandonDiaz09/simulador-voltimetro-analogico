1ff8b9583084af000468e4ecb3eb1e3fb49e2477
brandondiaz <branal845diaz@gmail.com>
2023-12-30 22:39:27 -0600

    Función de grafica para volmetro horizontal

--- volmetro.py
+++ volmetro.py
@@ -1,9 +1,43 @@
-import serial
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
 import random
-import time
+
+class VolmetroAnalogico:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Simulador de Voltímetro Analógico")
+        #tk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        # Configuración del gráfico
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        
+        # Inicialización del medidor
+        self.ax.set_xlim(0, 61)
+        self.ax.set_ylim(0, 1)
+        self.needle, = self.ax.plot([], [], 'r-')  # Aguja inicialmente vacía
+        # Dibujar el medidor
+        self.draw_meter()
+
+    def update_needle(self, value):
+        # Actualizar la aguja del medidor según el valor de voltaje
+        self.needle.set_data([value, value], [0, 0.5])
+        self.canvas.draw_idle()
+
+    def draw_meter(self):
+        # Dibujar el medidor con marcas
+        #self.ax.axis('off')
+        for i in range(61):
+            if i%10==0:
+                self.ax.plot([i, i], [0.75,1], 'k')  # Marcas del medidor
+            else:
+                self.ax.plot([i, i], [0.85,1], 'k')  # Marcas del medidor
+        self.ax.tick_params(axis='x', bottom=False,labeltop=True,labelbottom=False)
+        self.ax.tick_params(axis='y', left=False,labelleft=False)
+
 
 # Flag para controlar el modo de simulación
 SIMULATED = True
@@ -33,28 +67,22 @@ def leer_serial():
         ser = serial.Serial('COM3', 19200)  # Ajusta a tu configuración
         return leer_serial_real(ser)
 
-# Configurar la conexión serial
 
-# Configurar la ventana de Tkinter
-root = tk.Tk()
-root.title("Voltímetro Virtual")
+if __name__ == '__main__':
+    root = tk.Tk()
+    vm = VolmetroAnalogico(root)
+    # Función de actualización
+    def actualizar():
+        voltaje = leer_serial()
+        vm.update_needle(voltaje)
+        root.update()
+        root.after(1000, actualizar)  # Actualiza cada segundo
 
-# Configurar el gráfico de Matplotlib
-figura = Figure(figsize=(6, 5), dpi=100)
-sub_plot = figura.add_subplot(111)
-canvas = FigureCanvasTkAgg(figura, master=root)
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-# Función de actualización
-def actualizar():
-    voltaje = leer_serial()
-    sub_plot.clear()
-    sub_plot.plot(voltaje)  # Actualizar con el nuevo valor
-    canvas.draw()
-    root.after(1000, actualizar)  # Actualiza cada segundo
-
-# Iniciar la actualización
-actualizar()
-
-# Ejecutar el bucle principal
-root.mainloop()
+    #print("Hola")
+    # Simulando la actualización del voltaje
+    #for v in np.linspace(0, 10, 100):
+    #    vm.update_needle(leer_serial())
+    #    root.update()
+    #    root.after(1000)  # Espera un poco antes de actualizar
+    actualizar()
+    root.mainloop()
